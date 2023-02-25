ebd6eb79274d6141ab82af23732fe8a6276ccf59
Luis Felipe Rezende <lfsrsprofessional@gmail.com>
2023-02-25 01:14:43 -0300

    First Commit

---
+++ utils.py
@@ -0,0 +1,15 @@
+def isPositive(initialValue):
+    while True:
+        if initialValue <= 0:
+            initialValue = float(input("Please, type in a positive value."))
+        else:
+            break
+    return initialValue
+
+
+def initialValues(initialValue=None, interest=None):
+    if not initialValue:
+        
+       
+    if not interest:
+
